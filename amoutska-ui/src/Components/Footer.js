4b25bf9581796c19e91dba491d17fcaecf66c848
emrekocadere <emrekocadere34@gmail.com>
2025-01-27 18:25:12 +0300

    Feat: add Footer component with social media and contact icons

---
+++ amoutska-ui/src/Components/Footer.js
@@ -0,0 +1,21 @@
+import "../index.css";
+import { FacebookOutlined, MailOutlined, PhoneOutlined, InstagramOutlined, EnvironmentOutlined } from "@ant-design/icons";
+
+
+function Footer() {
+    return (
+        <div className="App">
+            <footer style={{ height: "12.5vh", backgroundColor: "blue" }}>
+                <div >
+                    <InstagramOutlined className="FooterIcon"/>
+                    <FacebookOutlined className="FooterIcon"/>
+                    <MailOutlined className="FooterIcon"/>
+                    <PhoneOutlined className="FooterIcon"/>
+                    <EnvironmentOutlined className="FooterIcon"/>
+                </div>
+
+            </footer>
+        </div>
+    );
+}
+export default Footer;
